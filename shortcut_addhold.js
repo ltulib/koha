c6b4be0ae2646eda00d4e615a5622c428e8b851d
ltulib <magnus.pettersson@ltu.se>
2018-05-16 09:48:18 +0200

    update

---
+++ shortcut_addhold.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    // Asks for search string (username, social security number etc.)
+    // it's hook on a register class click.
+    $(document.body).on('click', '.register', function(event) {
+        event.preventDefault();
+        var patreon = prompt("Enter patreon name or id", "");
+
+        if (patreon) {
+            var url = $(this).attr("href");
+            window.open(url + '&findborrower=' + patreon, '_blank');
+        }
+    });
+
+    // Add [add hold] button next to [add vendor note] at on open basket in Acquisitions
+    if ($('#acq_basket').length) {
+        $('#orders').on('init.dt', function() {
+            $('#orders').find('tbody').find('tr').each(function() {
+                var old = $(this).find("td:eq(1)").html();
+                var str = old.substr(old.indexOf('?'));
+                var start_pos = str.indexOf("=") + 1;
+                var end_pos = str.indexOf('"', start_pos);
+                var biblionumber = str.substring(start_pos, end_pos);
+
+                $(this).find("td:eq(1)").append("<span>[<a href='https://ils.lib.ltu.se/cgi-bin/koha/reserve/request.pl?biblionumber=" + biblionumber + "' class='register' target='_blank'>Add hold</a>]</span>");
+            });
+        });
+    }
+});
